e37d931c3d186db328df78050f0b0da1f3e2c747
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-22 18:37:12 +0530

    config nextra with section-theme-blog

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const withNextra = require("nextra")({
+  theme: "section-theme-blog",
+  themeConfig: "./theme.config.jsx",
+  defaultShowCopyCode: true,
+  readingTime: true,
+  codeHighlight: true,
+});
+
+module.exports = withNextra({
+  reactStrictMode: true,
+});
