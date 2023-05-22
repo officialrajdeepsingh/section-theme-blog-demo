d6c163a6e2b853c25041ca3999bdb1cec010890c
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-22 18:37:45 +0530

    create the theme.config file

---
+++ theme.config.jsx
@@ -0,0 +1,69 @@
+const themeConfig = {
+  logo: {
+    text: "Protfolio Blog",
+  },
+  primary_navigation: [
+    {
+      route: "/",
+      name: "Home",
+    },
+    {
+      route: "/posts",
+      name: "Blog",
+    },
+    {
+      route: "/authors",
+      name: "Authors",
+    },
+    {
+      route: "/about",
+      name: "About",
+    },
+    {
+      route: "/contact",
+      name: "Contact",
+    },
+  ],
+  secondary_navigation: [
+    {
+      route: "/disclaimer",
+      name: "Disclaimer",
+    },
+    {
+      route: "/privacy-policy",
+      name: "Privacy policy",
+    },
+    {
+      route: "/contact",
+      name: "Contact",
+    },
+  ],
+  social_links: [
+    {
+      name: "Facebook",
+      url: "https://facebook.com/officialrajdeepsingh",
+    },
+    {
+      name: "Twitter",
+      url: "https://facebook.com/officialrajdeepsingh",
+    },
+    {
+      name: "Linkedin",
+      url: "https://facebook.com/officialrajdeepsingh",
+    },
+    {
+      name: "Instagram",
+      url: "https://facebook.com/officialrajdeepsingh",
+    },
+    {
+      name: "Github",
+      url: "https://facebook.com/officialrajdeepsingh",
+    },
+    {
+      name: "facebook",
+      url: "https://facebook.com/officialrajdeepsingh",
+    },
+  ],
+};
+
+export default themeConfig;
